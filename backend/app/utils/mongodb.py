9cbb37f100b26250d034327e2748401a68d4118d
Pratik K <pkrockstar7@gmail.com>
2020-02-21 06:33:11 +0530

    [Addition] motor boilerplate

---
+++ backend/app/utils/mongodb.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+class Database:
+    client: AsyncIOMotorClient = None
+
+db = Database()
+
+async def get_database() -> AsyncIOMotorClient:
+    return db.client
+
